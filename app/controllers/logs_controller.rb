8058f6294f5fcd17e3e7be6695fa35e10c5e95c2
Chris Ryan <cryan@redhat.com>
2014-05-19 15:02:48 +0200

    Changed the log location.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,7 @@
 class LogsController < ApplicationController
   def index
     data_dir = ENV['OPENSHIFT_DATA_DIR']
-    @log_list = Dir.entries(data_dir)
+    log_dir = data_dir + "/log_repository"
+    @log_list = Dir.entries(log_dir)
   end
 end
