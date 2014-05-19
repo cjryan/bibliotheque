1ee9cfa49c30d0681a80645095cffde6a88889f2
Chris Ryan <cryan@redhat.com>
2014-05-19 16:43:49 +0200

    Ignore current and previous dirs in listing.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,11 +1,15 @@
 class LogsController < ApplicationController
   def index
+    @logs = []
     data_dir = ENV['OPENSHIFT_DATA_DIR']
     log_dir = data_dir + "/log_repository"
-    @log_list = Dir.entries(log_dir).each do |folder|
+    dir_listing = Dir.entries(log_dir)
+    dir_listing.each do |folder|
       #ignore current and previous dirs
-      next if folder == '.'
-      next if folder == '..'
+      next if folder == "."
+      next if folder == ".."
+      @logs << folder
+      return @logs
     end
   end
 end
