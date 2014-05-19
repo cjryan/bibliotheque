c1661aa015523016dc24e141a66181bbd5300549
Chris Ryan <cryan@redhat.com>
2014-05-19 14:54:40 +0200

    Fixed typo.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,6 @@
 class LogsController < ApplicationController
   def index
-    #data_dir = ENV[OPENSHIFT_DATA_DIR]
-    #@log_list = Dir.entries(data_dir)
+    data_dir = ENV['OPENSHIFT_DATA_DIR']
+    @log_list = Dir.entries(data_dir)
   end
 end
