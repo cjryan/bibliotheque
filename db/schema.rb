bc57302e5fc40e041a3c9a03def464a9ea4e8663
Chris Ryan <cryan@redhat.com>
2014-05-22 15:28:41 +0200

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520132858) do
+ActiveRecord::Schema.define(version: 20140520150726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "brokertypes", force: true do |t|
     t.string   "brokertype"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "dockerservers", force: true do |t|
+    t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
