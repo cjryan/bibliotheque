1c657fa14773db78fccc093ec5cbf1d26dd91ff9
Chris Ryan <cryan@redhat.com>
2014-05-28 18:00:13 +0200

    Added a logs view, changed the deploy hook to copy in the db.yml and secrets.yml file.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,4 +3,7 @@ class LogsController < ApplicationController
     file = Dir.glob(File.join('/home/ofayans/work/trololo/bibliotheque/app/views/static', 'log_repository', params["id"], "logs", "*"))[0]
     send_file(file, :disposition => 'inline', :type => "html")
   end
+  def index
+    @file = Dir.glob(File.join("#{ENV[OPENSHIFT_DATA_DIR]}/log_repository/*"))
+  end
 end
