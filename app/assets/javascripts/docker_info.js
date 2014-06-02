cc0a0163d7e7359e1fccdab6f3f37846ac4d87f5
Chris Ryan <cryan@redhat.com>
2014-06-02 12:45:30 +0200

    Added ajax support for multiple dropdowns concerning dockerservers and their images.

--- app/assets/javascripts/docker_info.js
+++ app/assets/javascripts/docker_info.js
@@ -1,20 +1,20 @@
 
-function query_dockerimages(server_id) {
-  var url = "/imageurls/" + server_id + ".json"
+function query_dockerimages(server_value, server_id) {
+  var url = "/imageurls/" + server_value + ".json"
   response = jQuery.getJSON(url)
   $.getJSON( url, function( data ) {
     var items = [];
     $.each( data, function( key, val ) {
       items.push(val);
     })
-    replace_guts(items)
+    replace_guts(items, server_id)
   });
   };
 
-function replace_guts(options) {
-  alert(options[0].tag)
-  
-  var $el = $("#run_image_url");
+function replace_guts(options, server_id) {
+  //alert(options[0].tag)
+  images = server_id.replace("dockerserver_id","image_id");
+  var $el = $("#"+images);
   $el.empty();
   $.each(options, function(key,value) {
   $el.append($("<option></option>")
